68cf4cea181e57a3961c0168989864180c1b6a86
Naresh Saharan <nareshsaharan2016@gmail.com>
2024-01-24 21:08:42 +0530

    basic of c

---
+++ 24Jan2024/formatspecifier.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+int main() {
+   
+   int a = 10;
+   int b = 20;
+   
+   //printf("%d", a);
+   //printf("%d %d", a, b);
+   printf("a:%d b:%d", a, b);
+   
+   return 1;
+}
